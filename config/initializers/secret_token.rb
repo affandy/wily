47040ad5dc5976765dec7732c5c59f3466c20a7b
Yanuar Affandy <affandy_89@yahoo.com>
2011-12-12 01:04:24 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wileaves::Application.config.secret_token = '2d70c404bf0750db318233c3cdfed05fc3ef57a81b3504e6a06b38146ff3525482488b9931c40336f4331583e83d67add3f1a29b70fd57c3d3c10747be8bbe20'
